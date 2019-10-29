40234ad02a5293071844d5048afbbd75887617e6
Antoine Braconnier <antoine.braconnier@hotmail.com>
2019-10-29 22:39:36 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+25.times do
+  real_text = []
+  10.times do
+    text = Faker::Lorem.paragraphs(number: 20)
+    text = text.join('')
+    real_text << ' ' + text + "\n"
+  end
+  real_text = real_text.join('')
+  new_article = Article.new(
+    title: Faker::TvShows::Friends.quote,
+    content: real_text
+  )
+  new_article.save
+end
